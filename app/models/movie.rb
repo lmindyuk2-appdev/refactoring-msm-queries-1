e4b25c07262001350d820115e2ec38436cf7c996
lmindyuk2 <lmindyuk@chicagobooth.edu>
2020-08-05 01:46:40 +0000

    added cast

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -22,4 +22,26 @@ class Movie < ApplicationRecord
 
     return the_director
   end
+
+  def cast
+    my_id = self.id
+    matching_characters = Character.where({ :movie_id => my_id}) 
+    array_actors = Array.new
+    matching_characters.each do |a_character| 
+      actor = Actor.where({ :id => a_character.actor_id })
+      array_actors = array_actors.push(actor)
+    end
+    cast = array_actors
+    return cast
+  end
+
+  def actors_names
+    actors_names = Array.new
+    self.cast.each do |an_actor|
+      actors_names = actors_names.push(an_actor.at(0).name)
+    end
+    actors_names = actors_names.join(", ")
+    return actors_names
+  end
+
 end
